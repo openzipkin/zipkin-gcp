f29d73e304ad19341371ec2ee319648c4902818a
elefeint <41136058+elefeint@users.noreply.github.com>
2018-10-05 10:02:47 -0400

    Fix style.

--- propagation-stackdriver/src/test/java/zipkin2/propagation/stackdriver/StackdriverTracePropagationTest.java
+++ propagation-stackdriver/src/test/java/zipkin2/propagation/stackdriver/StackdriverTracePropagationTest.java
@@ -24,45 +24,49 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StackdriverTracePropagationTest {
-	private static final String XCLOUD_VALUE = "c108dc108dc108dc108dc108dc108d00";
-	private static final String B3_HEADER = "b3";
-	private static final String B3_TRACE_ID = "b3b3b3b3b3b34da6a3ce929d0e0e4736";
-	private static final String B3_VALUE = String.format("%s-00f067aa0ba902b7-1", B3_TRACE_ID);
+  private static final String XCLOUD_VALUE = "c108dc108dc108dc108dc108dc108d00";
+  private static final String B3_HEADER = "b3";
+  private static final String B3_TRACE_ID = "b3b3b3b3b3b34da6a3ce929d0e0e4736";
+  private static final String B3_VALUE = String.format("%s-00f067aa0ba902b7-1", B3_TRACE_ID);
 
-	private Propagation<String> propagation = StackdriverTracePropagation.FACTORY.create(Propagation.KeyFactory.STRING);
+  private Propagation<String> propagation =
+      StackdriverTracePropagation.FACTORY.create(Propagation.KeyFactory.STRING);
 
-	@Test
-	public void b3TakesPrecedenceOverXCloud() {
-		TraceContext.Extractor<String> extractor = propagation.extractor(new FakeGetter(XCLOUD_VALUE, B3_VALUE));
-		TraceContextOrSamplingFlags ctx = extractor.extract("unused object");
-		assertThat(ctx.context().traceIdString()).isEqualTo(B3_TRACE_ID);
-	}
+  @Test
+  public void b3TakesPrecedenceOverXCloud() {
+    TraceContext.Extractor<String> extractor =
+        propagation.extractor(new FakeGetter(XCLOUD_VALUE, B3_VALUE));
+    TraceContextOrSamplingFlags ctx = extractor.extract("unused object");
+    assertThat(ctx.context().traceIdString()).isEqualTo(B3_TRACE_ID);
+  }
 
-	@Test
-	public void xCloudReturnedWhenB3Missing() {
-		TraceContext.Extractor<String> extractor = propagation.extractor(new FakeGetter(XCLOUD_VALUE, null));
-		TraceContextOrSamplingFlags ctx = extractor.extract("unused object");
-		assertThat(ctx.context().traceIdString()).isEqualTo(XCLOUD_VALUE);
-	}
+  @Test
+  public void xCloudReturnedWhenB3Missing() {
+    TraceContext.Extractor<String> extractor =
+        propagation.extractor(new FakeGetter(XCLOUD_VALUE, null));
+    TraceContextOrSamplingFlags ctx = extractor.extract("unused object");
+    assertThat(ctx.context().traceIdString()).isEqualTo(XCLOUD_VALUE);
+  }
 
-	@Test
-	public void b3ReturnedWhenXCloudMissing() {
-		TraceContext.Extractor<String> extractor = propagation.extractor(new FakeGetter(null, B3_VALUE));
-		TraceContextOrSamplingFlags ctx = extractor.extract("unused object");
-		assertThat(ctx.context().traceIdString()).isEqualTo(B3_TRACE_ID);
-	}
+  @Test
+  public void b3ReturnedWhenXCloudMissing() {
+    TraceContext.Extractor<String> extractor =
+        propagation.extractor(new FakeGetter(null, B3_VALUE));
+    TraceContextOrSamplingFlags ctx = extractor.extract("unused object");
+    assertThat(ctx.context().traceIdString()).isEqualTo(B3_TRACE_ID);
+  }
 
-	private static class FakeGetter implements Propagation.Getter<String,String> {
-		private Map<String,String> values = new HashMap<>();
+  private static class FakeGetter implements Propagation.Getter<String, String> {
+    private Map<String, String> values = new HashMap<>();
 
-		FakeGetter(String xCloudValue, String b3Value) {
-			this.values.put(StackdriverTracePropagation.TRACE_ID_NAME, xCloudValue);
-			this.values.put(B3_HEADER, b3Value);
-		}
+    FakeGetter(String xCloudValue, String b3Value) {
+      this.values.put(StackdriverTracePropagation.TRACE_ID_NAME, xCloudValue);
+      this.values.put(B3_HEADER, b3Value);
+    }
 
-		@Override
-		public String get(String carrier, String key) {
-			return values.get(key);
-		}
-	}
+    @Override
+    public String get(String carrier, String key) {
+      return values.get(key);
+    }
+  }
 }
