cf721b96a62212fbc479868fc24fb3b462be8a5f
Ray Tsang <saturnism@gmail.com>
2019-09-13 17:22:56 -0700

    Test with Zipkin Server

--- travis/publish.sh
+++ travis/publish.sh
@@ -104,6 +104,50 @@ safe_checkout_master() {
   fi
 }
 
+test_server() {
+  # Test Stackdriver Storage module with the latest version of Zipkin server.
+  temp_dir=$(mktemp -d)
+  pushd $temp_dir
+
+  # Download and unpack Zipkin Server
+  wget https://jitpack.io/com/github/openzipkin/zipkin/zipkin-server/master-SNAPSHOT/zipkin-server-master-SNAPSHOT-exec.jar
+
+  # Zipkin server JAR is a self-executable with headers that causes unzip to exit with status 1
+  # Fix the archive first.
+  zip -A zipkin-server-master-SNAPSHOT-exec.jar
+  unzip zipkin-server-master-SNAPSHOT-exec.jar -d zipkin
+
+  # Unpack our module
+  unzip -n $TRAVIS_BUILD_DIR/autoconfigure/storage-stackdriver/target/*-module.jar -d zipkin
+
+  # Start the server. Note that the GOOGLE_APPLICATION_CREDENTIALS is configured from .travis.yml
+  STORAGE_TYPE=stackdriver
+  STACKDRIVER_PROJECT_ID=zipkin-gcp-ci
+  java -Dloader.path=stackdriver -Dspring.profiles.active=stackdriver -cp zipkin/ org.springframework.boot.loader.PropertiesLauncher &
+  ZIPKIN_PID=$!
+
+  echo "Waiting for Zipkin server to start..."
+  $TRAVIS_BUILD_DIR/travis/wait-for-it.sh localhost:9411 -t 60
+  exit_status=$?
+  if [ $exit_status -ne 0 ]; then
+    exit $exit_status
+  fi
+
+  health_check_result=$(curl --silent localhost:9411/actuator/health| jq -r .details.zipkin.status)
+
+  if [ "$health_check_result" != "UP" ]; then
+    echo "Health check failed!"
+    curl --silent localhost:9411/actuator/health | jq .
+    exit 1
+  else
+    echo "Health check status is up!"
+  fi
+
+  kill -9 $ZIPKIN_PID
+
+  popd
+}
+
 #----------------------
 # MAIN
 #----------------------
@@ -119,6 +163,9 @@ if is_release_commit; then
   true
 else
   ./mvnw verify -nsu -Dlicense.skip=true
+
+  # TODO move to is_travis_branch_master after validating this runs in branch
+  test_server
 fi
 
 # If we are on a pull request, our only job is to run tests, which happened above via ./mvnw install
